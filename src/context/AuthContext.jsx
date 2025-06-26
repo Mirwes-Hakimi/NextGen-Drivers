4440bc382d34c93eaae17f7e31650bc54e875f98
Mirwes Hakimi <mirwes215@gmail.com>
2025-06-26 13:07:32 -0700

    created a useEffect function to set up listener track auth state changes login and logout

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -6,5 +6,23 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children}) => { ///provider component that wraps around the app
     const [user, setUser] = useState(null); /// state to store the currently logged-in user
+    
+    /// seting up listener to track auth state state changes login/logout
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, (user) => {
+         /// update the user state when auth state changes
+         setUser(user);
+        });
 
-}
+        /// cleanup listener when component unmounts
+        return () => unsub();
+    },[]);
+
+    return (
+        <AuthContext.Provider>
+        
+        </AuthContext.Provider>
+    );
+};
+/// Custom hook to easily access the AuthContext
+export const useAuth = () => useContext(AuthContext);
